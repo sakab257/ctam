3b706740961b9dd6d649aae9150df42c7fe51ace
Salim <slmbouskine93@gmail.com>
2026-01-09 16:30:25 +0100

    ajout de l'url rdv-controle-technique-moto-ivry.html pour le redirection du centre d'ivry

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         destination: "/",
         permanent: true,
       },
+      {
+        source: "/rdv-controle-technique-moto-ivry.html",
+        destination: "/",
+        permanent: true
+      }
     ]
   },
 };
